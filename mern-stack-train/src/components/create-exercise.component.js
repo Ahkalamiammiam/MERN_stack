16e9181aac3d1b27d1aa2715c51d26214ac0a239
Vianney Traina <vianney.traina@edu.devinci.fr>
2019-11-17 15:12:47 +0100

    Add React Router & Add Components

---
+++ mern-stack-train/src/components/create-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateExercise extends Component {
+    render() {
+        return (
+            <div>
+                <p>Bienvenue sur la CreateExercise Component !</p>
+            </div>
+        );
+    }
+}
+
+export default CreateExercise;
---
+++ mern-stack-train/src/components/edit-exercises.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>Bienvenue sur la EditExercises Component !</p>
+            </div>
+        );
+    }
+}
+
+export default EditExercises;
